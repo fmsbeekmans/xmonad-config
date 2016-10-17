838715471ad3c650d850423a8d5bfe297b7df059
Ferdy Beekmans <ferdy.beekmans@zivver.com>
2016-10-17 13:54:06 +0200

    BinarySpacePartition

--- xmonad.hs
+++ xmonad.hs
@@ -8,6 +8,8 @@ import XMonad.Actions.Submap
 import XMonad.Actions.CopyWindow as C
 import XMonad.Actions.CycleWS 
 import XMonad.Hooks.SetWMName
+import XMonad.Layout.BinarySpacePartition
+
 import System.Exit
 
 myModMask = mod5Mask
@@ -16,6 +18,7 @@ main = xmonad $ defaultConfig
   { workspaces = myWorkspaces
   , modMask = myModMask
   , startupHook = setWMName "LG3D"
+  , layoutHook = emptyBSP
   } `additionalKeys` myKeys
 
 myWorkspaces = ["0", "1", "2", "3", "4", "5", "6", "7"]
@@ -38,8 +41,13 @@ stdCommandKeys =
   , ((myModMask .|. shiftMask, xK_t)        , windows W.swapUp)
   , ((myModMask .|. shiftMask, xK_n)        , windows W.swapDown)
 
-  , ((myModMask .|. mod1Mask, xK_t)         , sendMessage Shrink)
-  , ((myModMask .|. mod1Mask, xK_n)         , sendMessage Expand)
+  , ((myModMask .|. mod1Mask, xK_h)         , sendMessage $ ExpandTowards L)
+  , ((myModMask .|. mod1Mask, xK_t)         , sendMessage $ ExpandTowards U)
+  , ((myModMask .|. mod1Mask, xK_n)         , sendMessage $ ExpandTowards D)
+  , ((myModMask .|. mod1Mask, xK_s)         , sendMessage $ ExpandTowards R)
+
+  , ((myModMask .|. mod1Mask, xK_r)         , sendMessage Rotate)
+  , ((myModMask .|. mod1Mask, xK_c)         , sendMessage FocusParent)
   ]
 
 screenKeys = 
@@ -51,38 +59,50 @@ screenKeys =
   , ((myModMask, xK_w)                      , windows (onlyOnScreen 1 "3"))
   , ((myModMask, xK_v)                      , windows (onlyOnScreen 1 "5"))
 
-  , ((myModMask .|. mod1Mask, xK_semicolon), windows (W.greedyView "4"))
-  , ((myModMask .|. mod1Mask, xK_q)        , windows (W.greedyView "2"))
-  , ((myModMask .|. mod1Mask, xK_j)        , windows (W.greedyView "0"))
+  , ((myModMask .|. mod1Mask, xK_semicolon) , windows (W.greedyView "4"))
+  , ((myModMask .|. mod1Mask, xK_q)         , windows (W.greedyView "2"))
+  , ((myModMask .|. mod1Mask, xK_j)         , windows (W.greedyView "0"))
 
-  , ((myModMask .|. mod1Mask, xK_m)        , windows (W.greedyView "1"))
-  , ((myModMask .|. mod1Mask, xK_w)        , windows (W.greedyView "3"))
-  , ((myModMask .|. mod1Mask, xK_v)        , windows (W.greedyView "5"))
+  , ((myModMask .|. mod1Mask, xK_m)         , windows (W.greedyView "1"))
+  , ((myModMask .|. mod1Mask, xK_w)         , windows (W.greedyView "3"))
+  , ((myModMask .|. mod1Mask, xK_v)         , windows (W.greedyView "5"))
     
-  , ((myModMask .|. mod4Mask, xK_semicolon), windows (W.shift "4"))
-  , ((myModMask .|. mod4Mask, xK_q)        , windows (W.shift "2"))
-  , ((myModMask .|. mod4Mask, xK_j)        , windows (W.shift "0"))
+  , ((myModMask .|. mod4Mask, xK_semicolon) , moveToWorkspaceAndFocus 0 "4")
+  , ((myModMask .|. mod4Mask, xK_q)         , moveToWorkspaceAndFocus 0 "2")
+  , ((myModMask .|. mod4Mask, xK_j)         , moveToWorkspaceAndFocus 0 "0")
 
-  , ((myModMask .|. mod4Mask, xK_m)        , windows (W.shift "1"))
-  , ((myModMask .|. mod4Mask, xK_w)        , windows (W.shift "3"))
-  , ((myModMask .|. mod4Mask, xK_v)        , windows (W.shift "5"))
+  , ((myModMask .|. mod4Mask, xK_m)         , moveToWorkspaceAndFocus 1 "1")
+  , ((myModMask .|. mod4Mask, xK_w)         , moveToWorkspaceAndFocus 1 "3")
+  , ((myModMask .|. mod4Mask, xK_v)         , moveToWorkspaceAndFocus 1 "5")
     
-  , ((myModMask, xK_k)                     , screenWorkspace 0 >>= flip whenJust (windows . W.view))
-  , ((myModMask, xK_b)                     , screenWorkspace 1 >>= flip whenJust (windows . W.view))
+  , ((myModMask, xK_k)                      , focusWorkspace 0)
+  , ((myModMask, xK_b)                      , focusWorkspace 1)
   ]
 
-spawnCommands =
-  [((myModMask, xK_a)                      , submap . M.fromList $
-      [ ((0, xK_o)                         , spawn "idea.sh")
-      , ((0, xK_e)                         , spawn "emacsclient -c")
-      , ((0, xK_u)                         , spawn "subl")
-      , ((0, xK_g)                         , spawn "chromium")
-      , ((shiftMask, xK_g)                 , spawn "conkeror")	
-      , ((0, xK_c)                         , spawn "urxvt")
-      , ((0, xK_r)                         , spawn "deluge")
-      , ((0, xK_m)                         , spawn "smplayer")
+focusWorkspace :: ScreenId -> X ()
+focusWorkspace s = do
+  w <- screenWorkspace s
+  flip whenJust (windows . W.view) w
 
-      , ((0, xK_period)                    , spawn "slock")
+moveToWorkspaceAndFocus :: ScreenId -> WorkspaceId -> X ()
+moveToWorkspaceAndFocus s w = do
+  windows (W.shift w)
+  windows (onlyOnScreen s w)
+  focusWorkspace s
+  
+spawnCommands =
+  [((myModMask, xK_a)                       , submap . M.fromList $
+      [ ((0, xK_o)                          , spawn "idea.sh")
+      , ((0, xK_e)                          , spawn "emacsclient -c")
+      , ((0, xK_u)                          , spawn "subl")
+      , ((0, xK_g)                          , spawn "chromium")
+      , ((shiftMask, xK_g)                  , spawn "conkeror")	
+      , ((0, xK_c)                          , spawn "urxvt")
+      , ((0, xK_r)                          , spawn "deluge")
+      , ((0, xK_m)                          , spawn "smplayer")
+
+      , ((0, xK_period)                     , spawn "slock")
       ]
     )
   ]
+
